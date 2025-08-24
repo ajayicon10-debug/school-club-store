9124b070dfd40d2bb82cc19235059b1c388b83e8
ajayicon10-debug <ajayicon10@gmail.com>
2025-08-24 13:54:01 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,42 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBKF93Hr8Wyf8ifSapvE6LsvwlVAboi2rA",
+    authDomain: "school-club-store.firebaseapp.com",
+    projectId: "school-club-store",
+    storageBucket: "school-club-store.firebasestorage.app",
+    messagingSenderId: "991285580248",
+    appId: "1:991285580248:web:dd32faed95508a3779a2ad"
+  };  
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
+
+
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKF93Hr8Wyf8ifSapvE6LsvwlVAboi2rA",
+  authDomain: "school-club-store.firebaseapp.com",
+  projectId: "school-club-store",
+  storageBucket: "school-club-store.firebasestorage.app",
+  messagingSenderId: "991285580248",
+  appId: "1:991285580248:web:dd32faed95508a3779a2ad"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
